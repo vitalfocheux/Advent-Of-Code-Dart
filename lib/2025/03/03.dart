a4a77f861ea6d1c3a702a96cbf1078c69ed74dc6
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2025-12-03 08:39:33 +0100

    Day 03

--- lib/2025/03/03.dart
+++ lib/2025/03/03.dart
@@ -7,12 +7,62 @@ void main() async {
   print(part2(aocd.asStr));
 }
 
+Iterable<List<T>> combinations<T>(List<T> items, int k) sync* {
+  final n = items.length;
+  if (k < 0 || k > n) return;
+  if (k == 0) {
+    yield <T>[];
+    return;
+  }
+  final indices = List<int>.generate(k, (i) => i);
+  while (true) {
+    yield indices.map((i) => items[i]).toList(growable: false);
+    var i = k - 1;
+    while (i >= 0 && indices[i] == i + n - k) {
+      i--;
+    }
+    if (i < 0) break;
+    indices[i]++;
+    for (var j = i + 1; j < k; j++) {
+      indices[j] = indices[j - 1] + 1;
+    }
+  }
+}
+
 dynamic part1(String input) {
-  // TODO: implémenter la solution de la partie 1
-  return null;
+  int res = 0;
+  for(String line in input.split('\n')){
+    List<int> numbers = line.split('').map(int.parse).toList();
+    List<int> nums = [];
+    for(var combs in combinations(numbers, 2)){
+      nums.add(combs[0] * 10 + combs[1]);
+    }
+    res += nums.max();
+  }
+  return res;
 }
 
 dynamic part2(String input) {
-  // TODO: implémenter la solution de la partie 2
-  return null;
+  num res = 0;
+  for(String line in input.split('\n')){
+    List<int> numbers = line.split('').map(int.parse).toList();
+    int start = 0;
+    int r = 0;
+    for(int i = 12; i > 0; i--){
+      
+      List<int> current;
+      if(i == -1){
+        current = numbers.sublist(start, numbers.length);
+      }else{
+        current = numbers.sublist(start, numbers.length -(i-1));
+      }
+      
+      int max = current.max();
+      start = numbers.indexOf(max, start) + 1;
+      r = r * 10 + max;
+      
+    }
+    res += r;
+  }
+  return res;
 }
